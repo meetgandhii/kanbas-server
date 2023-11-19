301d9c40bd967ae04ebc0e5e922376f2bf9e4f58
Meet Gandhi <meetgandhi@Meets-MacBook-Air.local>
2023-11-19 01:09:04 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import Hello from "./hello.js";
+import Lab5 from "./Lab5.js";
+import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./modules/routes.js";
+import "dotenv/config";
+
+const app = express();
+import cors from "cors";
+app.use(cors());
+app.use(express.json());
+ModuleRoutes(app);
+CourseRoutes(app);
+Lab5(app);
+Hello(app);
+app.listen(process.env.PORT || 4000);
+
